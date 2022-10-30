6410fc40ddf6c57547a0128e787c4a9bf4e447e3
yohash <jdheebl@gmail.com>
2022-10-30 21:49:30 +1300

    Added a class `NoProps` for easy-reference for a component with no props

--- Runtime/UnityReact/Component.cs
+++ Runtime/UnityReact/Component.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 namespace Yohash.React
 {
+  public class NoProps : Props
+  {
+    public override List<StateContainer> state => _state;
+    private List<StateContainer> _state = new List<StateContainer>();
+    public override void SetState(List<StateContainer> containers) { }
+  }
+
   public abstract class Props
   {
     public abstract List<StateContainer> state { get; }
