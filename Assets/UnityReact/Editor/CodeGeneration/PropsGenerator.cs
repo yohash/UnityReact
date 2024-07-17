ee660c69085ca7720058be442c2c1e7f99e6b3db
Jason Heebl <jdheebl@gmail.com>
2024-07-17 15:04:55 +1200

    Bugfix for code to generate if no namespace defined on targets

--- Assets/UnityReact/Editor/CodeGeneration/PropsGenerator.cs
+++ Assets/UnityReact/Editor/CodeGeneration/PropsGenerator.cs
@@ -39,8 +39,9 @@ namespace Yohash.React.Editor
       var namespaceGroups = propsTypes.GroupBy(t => t.Namespace);
 
       foreach (var namespaceGroup in namespaceGroups) {
+        if (namespaceGroup.Key == "Yohash.React") { continue; }
         var code = GeneratePropsMethodsCode(namespaceGroup.Key, namespaceGroup);
-        var name = namespaceGroup.Key.Split('.').Last();
+        var name = namespaceGroup.Key?.Split('.').Last() ?? Application.productName;
         var path = $"Assets/_Generated/{name}";
         var filename = $"{name}PropsMethods.cs";
         if (!Directory.Exists(path)) { Directory.CreateDirectory(path); }
@@ -59,11 +60,18 @@ namespace Yohash.React.Editor
 
     private static string GeneratePropsMethodsCode(string namespaceName, IEnumerable<Type> propsTypes)
     {
-      var name = namespaceName.Split('.').Last();
+      var name = namespaceName?.Split('.').Last() ?? Application.productName;
 
       var sb = new StringBuilder();
-      sb.AppendLine($"namespace {namespaceName}");
-      sb.AppendLine("{");
+      sb.AppendLine("using Yohash.React;");
+
+      bool noNamespace = namespaceName == null;
+
+      var b = noNamespace ? "" : "  ";
+      if (!noNamespace) {
+        sb.AppendLine($"namespace {namespaceName}");
+        sb.AppendLine("{");
+      }
 
       foreach (var propsType in propsTypes) {
         var propsName = propsType.Name;
@@ -76,77 +84,79 @@ namespace Yohash.React.Editor
           .Where(f => f.FieldType.IsSubclassOf(typeof(PropsContainer)))
           .ToList();
 
-        sb.AppendLine($"  public partial class {propsName} : Props");
-        sb.AppendLine("  {");
+        sb.AppendLine($"{b}public partial class {propsName} : Props");
+        sb.AppendLine($"{b}{{");
 
         // **** Generate class constructor
-        sb.AppendLine($"    public {propsName}()");
-        sb.AppendLine("    {");
+        sb.AppendLine($"{b}  public {propsName}()");
+        sb.AppendLine($"{b}  {{");
         foreach (var field in stateContainerFields) {
           var fieldName = field.Name;
           var fieldType = field.FieldType.Name;
-          sb.AppendLine($"      {fieldName} = new {fieldType}();");
+          sb.AppendLine($"{b}    {fieldName} = new {fieldType}();");
         }
         foreach (var container in propsContainerFields) {
           var fieldName = container.Name;
           var fieldType = container.FieldType.Name;
-          sb.AppendLine($"      {fieldName} = new {fieldType}();");
+          sb.AppendLine($"{b}    {fieldName} = new {fieldType}();");
         }
-        sb.AppendLine("    }");
+        sb.AppendLine($"{b}  }}");
         sb.AppendLine();
 
         // **** Generate BuildProps method
         if (stateContainerFields.Count > 0) {
-          sb.AppendLine($"    public override void BuildProps(State state)");
-          sb.AppendLine("    {");
-          sb.AppendLine($"      var _state = state as {name}State;");
+          sb.AppendLine($"{b}  public override void BuildProps(State state)");
+          sb.AppendLine($"{b}  {{");
+          sb.AppendLine($"{b}    var _state = state as {name}State;");
 
           foreach (var field in stateContainerFields) {
             var fieldName = field.Name;
             var fieldType = field.FieldType.Name;
-            sb.AppendLine($"      {fieldName} = _state.{fieldType};");
+            sb.AppendLine($"{b}    {fieldName} = _state.{fieldType};");
           }
-          sb.AppendLine("    }");
+          sb.AppendLine($"{b}  }}");
           sb.AppendLine();
         }
 
         // **** Generate DidUpdate method
-        sb.AppendLine($"    public override bool DidUpdate()");
-        sb.AppendLine("    {");
+        sb.AppendLine($"{b}  public override bool DidUpdate()");
+        sb.AppendLine($"{b}  {{");
 
         if (stateContainerFields.Count > 1) {
-          sb.AppendLine($"      return {stateContainerFields[0].Name}.IsDirty ||");
+          sb.AppendLine($"{b}    return {stateContainerFields[0].Name}.IsDirty ||");
           for (var i = 1; i < stateContainerFields.Count; i++) {
             var fieldName = stateContainerFields[i].Name;
-            var line = $"        {fieldName}.IsDirty" + (i == stateContainerFields.Count - 1 ? ";" : " ||");
+            var line = $"{b}      {fieldName}.IsDirty" + (i == stateContainerFields.Count - 1 ? ";" : " ||");
             sb.AppendLine(line);
           }
         } else if (stateContainerFields.Count == 1) {
-          sb.AppendLine($"      return {stateContainerFields[0].Name}.IsDirty;");
+          sb.AppendLine($"{b}    return {stateContainerFields[0].Name}.IsDirty;");
         } else if (stateContainerFields.Count == 0) {
-          sb.AppendLine($"      return true;");
+          sb.AppendLine($"{b}    return true;");
         }
         sb.AppendLine("    }");
 
         // **** Generate BuildElement method
         if (propsContainerFields.Count > 0) {
           sb.AppendLine();
-          sb.AppendLine($"    public override void BuildElement(PropsContainer propsContainer)");
-          sb.AppendLine("    {");
+          sb.AppendLine($"{b}  public override void BuildElement(PropsContainer propsContainer)");
+          sb.AppendLine($"{b}  {{");
           // currently, only one props container is supported
           var fieldName = propsContainerFields[0].Name;
           var fieldType = propsContainerFields[0].FieldType.Name;
-          sb.AppendLine($"      {fieldName} = propsContainer as {fieldType};");
-          sb.AppendLine("    }");
+          sb.AppendLine($"{b}    {fieldName} = propsContainer as {fieldType};");
+          sb.AppendLine($"{b}  }}");
         }
 
-        sb.AppendLine("  }");
+        sb.AppendLine($"{b}}}");
         if (propsType != propsTypes.Last()) {
           sb.AppendLine();
         }
       }
 
-      sb.AppendLine("}");
+      if (!noNamespace) {
+        sb.AppendLine("}");
+      }
 
       return sb.ToString();
     }
