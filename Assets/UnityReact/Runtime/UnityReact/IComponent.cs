1beddffc887b3bcbdc0a092ce2af8a0e39edf755
Jason Heebl <jdheebl@gmail.com>
2024-10-26 15:12:01 +1300

    Modify `Icomponent` with an `InitializeElement(PropsContainer, State)` to properly assemble element props and initialize.
    The associated method was created in `Component` as well, and will (1) build props, (2) build element (`PropsContainer`) and (3) call `InitializeComponent()`

--- Assets/UnityReact/Runtime/UnityReact/IComponent.cs
+++ Assets/UnityReact/Runtime/UnityReact/IComponent.cs
@@ -6,8 +6,8 @@ namespace Yohash.React
   {
     Transform Transform { get; }
     UnityEngine.Object Object { get; }
+    void InitializeElement(PropsContainer props, State state);
     void UpdateElementWithProps(PropsContainer props, State state);
     void Unmount();
   }
 }
-
